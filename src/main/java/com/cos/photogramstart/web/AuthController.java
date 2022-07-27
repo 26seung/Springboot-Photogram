e3a5c17056152923cfb7c7e53a1edd28b48e7d1f
Euseung <yousi7@naver.com>
2022-07-28 01:57:03 +0900

    controller

--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -1,11 +1,16 @@
 package com.cos.photogramstart.web;
 
+import com.cos.photogramstart.web.dto.auth.SignupDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller //  1.IOC , 2.파일을 리턴하는 컨트롤러
 public class AuthController {
+
+    private static final Logger log = LoggerFactory.getLogger( AuthController.class );
 
     @GetMapping("/auth/signin")
     public String signinForm(){
@@ -16,8 +21,8 @@ public class AuthController {
         return "auth/signup";
     }
     @PostMapping("/auth/signup")
-    public String signup(){
-        System.out.println("POST 요청(signUp)");
+    public String signup(SignupDto signupDto){
+        log.info(signupDto.toString());
         return "auth/signin";
     }
 }

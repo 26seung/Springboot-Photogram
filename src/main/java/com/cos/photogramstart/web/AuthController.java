a5fa237847958c76fb735f49abfce35f1b8b1a26
Euseung <yousi7@naver.com>
2022-07-31 12:46:17 +0900

    AOP

--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -1,6 +1,7 @@
 package com.cos.photogramstart.web;
 
 import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.handler.ex.CustomValidationException;
 import com.cos.photogramstart.service.AuthService;
 import com.cos.photogramstart.web.dto.auth.SignupDto;
 import lombok.RequiredArgsConstructor;
@@ -8,8 +9,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor        // final 필드 DI 할때 사용
 @Controller //  1.IOC , 2.파일을 리턴하는 컨트롤러
@@ -29,12 +36,28 @@ public class AuthController {
         return "auth/signup";
     }
     @PostMapping("/auth/signup")
-    public String signup(SignupDto signupDto){
+    public String signup(@Valid SignupDto signupDto, BindingResult bindingResult){
+
+
+        if(bindingResult.hasErrors()){
+            Map<String, String >errMap = new HashMap<>();
+
+            for(FieldError error:bindingResult.getFieldErrors()){
+                errMap.put(error.getField(),error.getDefaultMessage());
+                System.out.println("bindingResult: " + error.getDefaultMessage());
+            }
+            throw new CustomValidationException("유효성검사 실패함", errMap);
+        }else {
         log.info(signupDto.toString());
         User user = signupDto.toEntity();
         log.info(user.toString());
+
         User userEntity = authService.회원가입(user);
         log.info(userEntity.toString());
+
         return "auth/signin";
+
+        }
+
     }
 }
--- src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
+++ src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
@@ -3,14 +3,23 @@ package com.cos.photogramstart.web.dto.auth;
 import com.cos.photogramstart.domain.user.User;
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 // 통신할때 필요한 데이터를 담아두는 오브젝트  (Data Transfer Object)
 // 도메인을 사용하게 되면 필요하지 않은 데이터까지 노출이 될 수 있는 우려가 있다.
 // dto 를 사용하면 domain 을 캡슐화 할 수 있다.
 @Data   // Getter, Setter
 public class SignupDto {
+    @Size(min = 2,max = 20)
+    @NotBlank
     private String username;
+    @NotBlank
     private String password;
+    @NotBlank
     private String email;
+    @NotBlank
     private String name;
 
     public User toEntity(){

f07dbf2bcaefef82a2789eb95c25044461fc4e36
Euseung <yousi7@naver.com>
2022-08-01 20:16:35 +0900

    login

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -1,5 +1,9 @@
 package com.cos.photogramstart.web;
 
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +16,16 @@ public class UserController {
         return "user/profile";
     }
     @GetMapping("/user/{id}/update")
-    public String update(@PathVariable int id){
+    public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails){
+        // 1. 어노테이션 PrincipalDetails 을 사용하는 것이 유용하다.
+        System.out.println("세션 정보: " + principalDetails.getUser());
+
+        // 2. 직접 세션 정보를 찾으려면 다음과 같은 방식으로 찾아 볼 수 있다.
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("직접 찾은 세션 정보: " + auth.getPrincipal());
+
+        PrincipalDetails mPrincipalDetails1 = (PrincipalDetails) auth.getPrincipal();
+        System.out.println("직접 찾은 세션 정보 2: "+ mPrincipalDetails1.getUser());
         return "user/update";
     }
 }

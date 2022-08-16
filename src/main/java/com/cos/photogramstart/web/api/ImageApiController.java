263274997184e166eddb6259e662a252a146032f
Euseung <yousi7@naver.com>
2022-08-17 00:49:01 +0900

    imageView

--- src/main/java/com/cos/photogramstart/web/api/ImageApiController.java
+++ src/main/java/com/cos/photogramstart/web/api/ImageApiController.java
@@ -5,6 +5,9 @@ import com.cos.photogramstart.domain.image.Image;
 import com.cos.photogramstart.service.ImageService;
 import com.cos.photogramstart.web.dto.CMRespDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -21,8 +24,9 @@ public class ImageApiController {
 
 
     @GetMapping("/api/image")
-    public ResponseEntity<?> imageStory(@AuthenticationPrincipal PrincipalDetails principalDetails){
-        List<Image> images = imageService.이미지스토리(principalDetails.getUser().getId());
+    public ResponseEntity<?> imageStory(@AuthenticationPrincipal PrincipalDetails principalDetails,
+                                        @PageableDefault(size=3, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+        List<Image> images = imageService.이미지스토리(principalDetails.getUser().getId(), pageable);
         return new ResponseEntity<>(new CMRespDto<>(1,"성공",images), HttpStatus.OK);
     }
 }

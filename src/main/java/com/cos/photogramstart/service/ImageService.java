2f68226672791aa6fa3406f2619d40d96ff62533
Euseung <yousi7@naver.com>
2022-08-17 01:06:05 +0900

    pageable

--- src/main/java/com/cos/photogramstart/service/ImageService.java
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -6,6 +6,7 @@ import com.cos.photogramstart.domain.image.ImageRepository;
 import com.cos.photogramstart.web.dto.image.ImageUploadDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,7 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -22,8 +22,8 @@ public class ImageService {
 
     private final ImageRepository imageRepository;
     @Transactional(readOnly = true)     //  영속성 컨텍스트 변경 감지를 해서, 더치 체킹, Flush(반영) 을 하지 않음,  트랜잭션을 사용하는 이유는 세션을 컨트롤러 단까지 끌고 오기 위해서
-    public List<Image> 이미지스토리(int principalId, Pageable pageable){
-        List<Image> images = imageRepository.mStory(principalId, pageable);
+    public Page<Image> 이미지스토리(int principalId, Pageable pageable){
+        Page<Image> images = imageRepository.mStory(principalId, pageable);
         return images;
     }
 

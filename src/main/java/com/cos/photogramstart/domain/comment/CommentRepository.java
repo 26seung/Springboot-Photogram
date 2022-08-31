9eec23bd7969690f96b48d7faa3ae4d289104b94
Euseung <yousi7@naver.com>
2022-08-31 23:15:57 +0900

    comment

--- src/main/java/com/cos/photogramstart/domain/comment/CommentRepository.java
+++ src/main/java/com/cos/photogramstart/domain/comment/CommentRepository.java
@@ -1,13 +1,12 @@
 package com.cos.photogramstart.domain.comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 public interface CommentRepository extends JpaRepository<Comment,Integer> {
 
-    @Modifying
-    @Query(value = "INSERT INTO comment(content, imageId, userId, createDate) VALUES (:content, :imageId, :userId, now())", nativeQuery = true)
-    Comment mSave(String content, int imageId, int userId);
+    // Comment 타입으로 입력이 되지 않음 int 형식을 사용해야 하나 그러면 Comment 의 id 값을 읽을수는 없다
+//    @Modifying
+//    @Query(value = "INSERT INTO comment(content, imageId, userId, createDate) VALUES (:content, :imageId, :userId, now())", nativeQuery = true)
+//    Comment mSave(String content, int imageId, int userId);
 
 }
--- src/main/java/com/cos/photogramstart/service/CommentService.java
+++ src/main/java/com/cos/photogramstart/service/CommentService.java
@@ -4,6 +4,8 @@ import com.cos.photogramstart.domain.comment.Comment;
 import com.cos.photogramstart.domain.comment.CommentRepository;
 import com.cos.photogramstart.domain.image.Image;
 import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.domain.user.UserRepository;
+import com.cos.photogramstart.handler.ex.CustomApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,26 +15,31 @@ import org.springframework.transaction.annotation.Transactional;
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
 
 
     @Transactional
     public Comment 댓글쓰기(String content, int imageId, int userId){
 
         // 객체를 만들 때 id 값만 담아서 insert 할 수 있다.
+        // 대신 return 시에 image 객체와 user 객체는 id 값만 가지고 있는 빈 객체를 리턴받는다.
         Image image = new Image();
         image.setId(imageId);
 
-        User user = new User();
-        user.setId(userId);
+        User userEntity = userRepository.findById(userId).orElseThrow(()->{
+            throw new CustomApiException("유저 아이디를 찾을 수 없습니다.");
+        });
+//        User user = new User();
+//        user.setId(userId);
 
 
         Comment comment = new Comment();
         comment.setContent(content);
         comment.setImage(image);
-        comment.setUser(user);
+        comment.setUser(userEntity);
 
-        commentRepository.mSave(content, imageId, userId);
-        return null;
+//        commentRepository.mSave(content, imageId, userId);
+        return commentRepository.save(comment);
     }
     @Transactional
     public void 댓글삭제(){
